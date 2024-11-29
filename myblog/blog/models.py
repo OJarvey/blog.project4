863b53226af51c551c456ab663ed51038c3a5ae9
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-11-29 22:12:10 +0000

    Rename Blog model to Post and add created, updated fields with ordering

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.utils import timezone
-
-class Blog(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ('-publish',)
     def __str__(self):
         return self.title
