01a204a888054b88f0321f45cc8815e0c9ca8599
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-12-06 17:51:04 +0000

    Refactor Post model and views for improved readability and consistency; update URL patterns for post detail view

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -3,42 +3,42 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
+
 class Post(models.Model):
-    
+
     objects = models.Manager()
     published = PublishedManager()
+
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='blog_posts'
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
     )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
-        max_length=2,
-        choices=Status.choices,
-        default=Status.DRAFT
+        max_length=2, choices=Status.choices, default=Status.DRAFT
     )
 
     class Meta:
-        ordering = ['-publish']
+        ordering = ["-publish"]
         indexes = [
-            models.Index(fields=['-publish']),
+            models.Index(fields=["-publish"]),
         ]
 
     def __str__(self):
         return self.title
+
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse("blog:post_detail", args=[self.id])
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from .models import Post
 
+
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, "blog/post/list.html", {"posts": posts})
 
-def post_detail(request, id):
-    post = get_object_or_404( Post, id=id, status=Post.Status.PUBLISHED )
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    return render(request, "blog/post/detail.html", {"post": post})
