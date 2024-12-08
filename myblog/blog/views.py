58631d4c0e3af6e1970775c6b142fa466ac1721d
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-12-08 21:41:22 +0000

    Add email sharing functionality to post detail view

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -33,10 +34,26 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
 
+    sent = False
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']}) " f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
 
     else:
         form = EmailPostForm()
