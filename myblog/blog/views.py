3c3c8b0bb8c0ae8000f44cc423b4ede2ba54445e
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-12-11 16:57:47 +0000

    Add view for posting comments on published blog posts

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,10 +1,21 @@
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
 
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, "blog/post/detail.html", {"post": post, "form": form, "comment": comment})
 
 def post_list(request):
     posts_list = Post.published.all()
