6546f7a2d818572dbf5fa691e3823902aa173511
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-12-14 20:00:17 +0000

    Set DEBUG to False and configure ALLOWED_HOSTS for production deployment

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -19,9 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c#aba7rr(7@lm$+n2+v1s+(qfgsj1x*4$%+6rm+uv_v1dce_bc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 SITE_ID = 1
 
