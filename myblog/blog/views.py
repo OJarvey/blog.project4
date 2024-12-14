7f9b7adc38abc0326f278cc57b6f3948c26d771d
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-12-14 18:53:06 +0000

    Enhance search functionality by implementing ranking and query support using PostgreSQL's full-text search features

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -9,6 +9,12 @@ from taggit.models import Tag
 from django.db.models import Count
 from django.contrib.postgres.search import SearchVector
 
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+)
+
 @require_POST
 def post_comment(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
@@ -91,9 +97,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            results = Post.published.annotate(
-                search=SearchVector("title", "body")
-            ).filter(search=query)
+            search_vector = SearchVector('title','body')
+            search_query = SearchQuery(query)
+
+            results =(
+                Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+            )
     return render(request, "blog/post/search.html", {"form": form, "query": query, "results": results})
 
 
