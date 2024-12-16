9ef7dcf34e20788692a21f9058832da7fe3f640b
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-12-16 16:40:41 +0000

    Set DEBUG to False for production readiness

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c#aba7rr(7@lm$+n2+v1s+(qfgsj1x*4$%+6rm+uv_v1dce_bc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
