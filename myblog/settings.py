d7a6e4062a0a1173e1ed2b51d9d867eb6f3b80b6
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-12-28 01:32:56 +0000

    Update settings to disable debug mode and add new allowed host for deployment

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,9 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ('av_n^pmol%8=td+wlswh7ik_!irnfmefglgps$p4@r%i0q*rpl')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'blog-project4.herokuapp.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'blog-project4.herokuapp.com',
+    'blog-project4-fcfadc1fce94.herokuapp.com',
+]
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
