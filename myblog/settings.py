90e6a023e46266c63bbbddb75d799d944c40abb4
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-12-29 19:18:46 +0000

    Disable debug mode for production environment

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ('av_n^pmol%8=td+wlswh7ik_!irnfmefglgps$p4@r%i0q*rpl')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
