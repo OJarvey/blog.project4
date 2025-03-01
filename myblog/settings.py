58d340e8471e1af4f69b9e1154379b03366e5355
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-03-01 00:27:43 +0000

    Refactor authentication templates to remove unnecessary 'allauth' load statements and update static file paths in settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     "django.contrib.sitemaps",
     "django.contrib.postgres",
     "taggit",
+    'whitenoise.runserver_nostatic',
     "cloudinary_storage",
     "cloudinary",
     "blog.apps.BlogConfig",
@@ -150,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "blog/static"]
+STATICFILES_DIRS = [BASE_DIR / "blog" / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
