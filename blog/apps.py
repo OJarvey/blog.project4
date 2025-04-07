c768a5ae498396df6911d3292d95a05be92b1b30
OJarvey <165787104+OJarvey@users.noreply.github.com>
2025-04-08 00:01:36 +0100

    Update apps.py

--- blog/apps.py
+++ blog/apps.py
@@ -7,8 +7,3 @@ class BlogConfig(AppConfig):
 
     def ready(self):
         import blog.signals  # noqa F401
-        from django.contrib.sites.models import Site
-        Site.objects.filter(id=1).update(
-            name="Blog Soul",
-            domain="127.0.0.1:8000"
-        )
